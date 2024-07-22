fe58d76cd5228bdad2eee6ea275b306b978931e8
MuteAvery <107277836+Retro023@users.noreply.github.com>
2024-07-22 14:47:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Function to print messages in color
+
 print_color() {
     local color="$1"
     local message="$2"
@@ -20,12 +20,12 @@ print_color() {
     esac
 }
 
-# Function to check if a command exists
+
 check_command() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Function to check if the program is already installed
+
 check_program_installed() {
     if [ -f /usr/local/bin/calculator ]; then
         return 0
@@ -34,7 +34,7 @@ check_program_installed() {
     fi
 }
 
-# Check if the program already exists
+
 if check_program_installed; then
     print_color "yellow" "The program 'calculator' is already installed."
     read -p "Do you want to overwrite it? (y/n) " choice
@@ -44,17 +44,17 @@ if check_program_installed; then
     fi
 fi
 
-# Check for wget
+
 if check_command wget; then
     print_color "green" "wget is installed, starting download"
-    wget -O /usr/local/bin/calculator https://github.com/Retro023/Calculator/raw/main/calculator
+    wget -O /usr/local/bin/calculator https://github.com/Retro023/Calculator/raw/main/Dist/calculator3_0
     if [ $? -eq 0 ]; then
         print_color "green" "Download successful"
         
-        # Make the file executable
+        
         chmod +x /usr/local/bin/calculator
 
-        # Add to PATH if not already in PATH
+        
         if [[ ":$PATH:" != *":/usr/local/bin:"* ]]; then
             echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
             source ~/.bashrc
